5c53e7ae075d5daead697326e98f28643660e0f0
phuongtri1909 <tri2003bt@gmail.com>
2025-09-23 01:57:30 +0700

    fix add styles admin

--- resources/views/admin/pages/auth/login.blade.php
+++ resources/views/admin/pages/auth/login.blade.php
@@ -23,7 +23,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
     <!-- Custom CSS -->
-    @vite('resources/assets/admin/css/styles.css')
     @vite('resources/assets/frontend/css/auth.css')
 
 </head>
